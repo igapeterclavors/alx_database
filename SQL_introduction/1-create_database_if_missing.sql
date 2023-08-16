e7a15b160a1b4dbf218a9fd0bd8ca30713910039
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-16 21:32:48 +0300

    adding task1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,8 @@
 -- Command to create the database hbtn_0c_0
 
-CREATE DATABASE hbtn_0c_0
+-- CREATE DATABASE hbtn_0c_0
+
+-- Delete and create database hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
