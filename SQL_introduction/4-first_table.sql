e0c3b7cc282ee0ba8deb733e908b93540094b38b
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-16 21:55:56 +0300

    adding task4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
 -- Delete and creates a table called first_table
 DROP TABLE IF EXISTS first_table;
-CREATE TABLE IF NOT EXISTS first_table;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
