93d9b979670105b7866cac1c00ed34d2c7b18b29
igapeterclavors <iga.peterclavors@gmail.com>
2023-08-23 18:39:29 +0300

    adding task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,8 @@
 -- list all privileges MySQL users user_0d_1 and user_0d_2
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
+
